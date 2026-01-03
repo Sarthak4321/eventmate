23dd3c3cc9456356cfb25c66127d1fbd3a31ebf9
Sarthak4321 <roysarthak16@gmail.com>
2026-01-03 22:12:22 +0530

    Configure serverExternalPackages for libsql

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ['@libsql/client', '@prisma/adapter-libsql', 'libsql'],
 };
 
 module.exports = nextConfig;
